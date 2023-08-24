fd46d945657d2f74a967bb4afe3414cc35d38a38
StruggsJ <strjac5@gmail.com>
2023-08-24 15:01:03 -0500

    Filled in template.

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myString = "This string of words";
+            int numberWords = 3;
+            char startingLetter = 'J';
+            bool isTrue = true;
+            double myDouble = 12.60;
+            decimal maxSpend = 30.00m;
+
+            Console.WriteLine($"{myString} has more than {numberWords} words.");
+            //To-Do: See if it is possible to convert boolean to string in order to force lowercasing via ToLower()
+            Console.WriteLine($"While it is {isTrue} that my name starts with the letter {startingLetter}," +
+                $" I do not want to spend more than ${maxSpend} for sheets of paper."); 
+            Console.WriteLine($"For ${myDouble}, I can buy a handful of snacks, however.");
         }
     }
 }
